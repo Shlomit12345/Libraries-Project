cb39fed084b873fadd3bc3d900cbc4c32032b6a5
Shlomit <s52005abc@gmail.com>
2018-04-18 22:28:23 +0300

    Add navbarCtrl.js + add it into index.html+ fix navbar.html

---
+++ app/navbar/navbarCtrl.js
@@ -0,0 +1,9 @@
+app.controller('navbarCtrl', function($scope, activeUserService, $location) {
+    
+    $scope.logout = function() {
+        activeUserService.logout();
+        $location.path('/');
+    }
+
+
+})
