81c155169de0bed362d4025c17edc51247b78079
Shlomit <s52005abc@gmail.com>
2018-05-13 12:29:31 +0300

    Bug fixing - preventing from saving  abook which is missing book name, author or publisher

--- app/books/booksCtrl.js
+++ app/books/booksCtrl.js
@@ -15,12 +15,21 @@ app.controller('booksCtrl', function($scope, activeUserService, $location, bookS
 	}
 
 	$scope.saveBook = function() {
-		bookService.saveBook($scope.bookname, $scope.bookauthor, $scope.coverimg, $scope.publish);
-		$scope.bookname = null;
-		$scope.bookauthor = null;
-		$scope.coverimg = null;
-		$scope.publish = null;
-		$("#myModal").modal("hide");
+		if ($scope.bookname == null || $scope.bookname == "" || $scope.bookauthor == null || $scope.bookauthor == "" || $scope.publish == null || $scope.publish == "") {
+			$scope.missingBookName = true;
+		} else {
+			bookService.saveBook($scope.bookname, $scope.bookauthor, $scope.coverimg, $scope.publish);
+			$scope.bookname = null;
+			$scope.bookauthor = null;
+			$scope.coverimg = null;
+			$scope.publish = null;
+			$scope.missingBookName = false;
+			$("#myModal").modal("hide");
+		}
+	}
+
+	$scope.closeError = function() {
+	   $scope.missingBookName = false;
 	}
 
     if (activeUserService.getUser().role === "ספרנית") {
